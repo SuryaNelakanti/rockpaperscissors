21281d793a84a5d4d86ddc6c71fe4ebe4f5f3970
Suryateja Nelakanti <47491728+SuryaNelakanti@users.noreply.github.com>
2020-04-14 12:19:05 +0530

    Update rock.py

--- rock.py
+++ rock.py
@@ -1,6 +1,6 @@
 import random
-print("Welcome to Rock Paper Scissors\n")
-def rocker():
+print("Welcome to Rock Paper Scissors\n")#A simple Intro
+def rocker():#Made the taking of input a function so i can call it recursively
 	sele=input("Enter R for Rock \nEnter P for paper\nEnter S for scissors\n")
 	global sel
 	sel = sele.upper()
@@ -9,7 +9,7 @@ def rocker():
 		rocker()
 	else:"""
 	ai()	
-def ai():
+def ai():# A simple code to make computer choose between rock paper and scissor
 	ai = random.choice([1,2,3])
 	global comp
 	if ai==1:
@@ -19,7 +19,7 @@ def ai():
 	else: 
 		comp = "S"
 	result()
-def result():
+def result():# comparing user input and computer selection and printing result
 	print("User has given " +sel + "\nComputer has given " + comp)
 	if sel==comp:
 		print("\nIt is a draw\n")
